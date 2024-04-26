f80d8472a061cdd716a67e12fa171b76f696e5e3
ceastld <1950946176@qq.com>
2024-04-26 23:58:14 +0800

    fix

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-https://github.com/harlanhong/ICCV2023-MCNET
+see https://github.com/harlanhong/ICCV2023-MCNET
 
 conda create -n mcnet -y python=3.10
 conda activate mcnet
@@ -8,4 +8,3 @@ pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
 pip install -r requirements.txt
 cd face-alignment
 pip install -r requirements.txt
-python setup.py install
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
-CUDA_VISIBLE_DEVICES=0 python demo.py  --config config/vox-256.yaml \
-    --driving_video video/input.mp4 --source_image video/src.png \
+#!/bin/bash
+
+# 定义变量
+name="zcl"
+mkdir -p video/mcnet/zcl1
+
+CUDA_VISIBLE_DEVICES=5 python demo.py  --config config/vox-256.yaml \
+    --driving_video video/gyd/frontal1.mp4 \
+    --source_image video/ids/zcl/zcl_ffhq.jpg \
+    --result_video video/mcnet/zcl1/output.mp4 \
     --checkpoint 00000099-checkpoint.pth.tar --relative --adapt_scale --kp_num 15 \
-    --generator Unet_Generator_keypoint_aware \
-    --result_video video/output.mp4 --mbunit ExpendMemoryUnit --memsize 1 
+    --generator Unet_Generator_keypoint_aware --mbunit ExpendMemoryUnit --memsize 1
+    
